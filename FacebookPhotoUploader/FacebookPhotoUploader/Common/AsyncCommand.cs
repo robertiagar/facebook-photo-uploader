6f58f5aa7b16cbc6831e021cf0bd24246d1c1533
Robert Iagar <robert.iagar@gmail.com>
2015-07-08 17:19:44 +0300

    Adding AsyncCommand

---
+++ FacebookPhotoUploader/FacebookPhotoUploader/Common/AsyncCommand.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FacebookPhotoUploader.Common
+{
+    public class AsyncCommand : ICommand
+    {
+        private readonly Func<Task> asyncAction;
+        private bool isRunning = false;
+
+        public bool IsRunning
+        {
+            get { return isRunning; }
+            set
+            {
+                isRunning = value;
+                if (CanExecuteChanged != null)
+                {
+                    CanExecuteChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        public AsyncCommand(Func<Task> asyncAction)
+        {
+            this.asyncAction = asyncAction;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return !isRunning;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public async void Execute(object parameter)
+        {
+            IsRunning = true;
+            await asyncAction();
+            IsRunning = false;
+        }
+    }
+}
